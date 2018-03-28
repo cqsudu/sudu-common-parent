00de0f12f39b243187a35b7965ca2e97ed8efebd
ikonghang <563796329@qq.com>
2018-03-28 13:20:20 +0800

    增加常量

--- sudu-base-core/src/main/java/com/ledo/core/exceptions/BaseExceptionConstants.java
+++ sudu-base-core/src/main/java/com/ledo/core/exceptions/BaseExceptionConstants.java
@@ -4,6 +4,8 @@ public interface BaseExceptionConstants {
     interface StartCode{
         long COMMON_EXCEPTION_START_CODE = 10000; //公共错误code开始10000结束于11999
         long OAUTH_EXCEPTION_START_CODE = 11000; //oauth错误code开始11000结束于11999
+        long SYSUSER_EXCEPTION_START_CODE = 12000; //SysUser错误code开始11000结束于11999
+        long USER_EXCEPTION_START_CODE = 13000; //User错误code开始11000结束于11999
     }
 
     long getCode();
